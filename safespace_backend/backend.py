a7e0a636edf648bc6dc98f6eb5dd9cdff0cf0ee5
alexwonseokcho <wonseok4840@gmail.com>
2023-01-29 00:53:31 -0500

    added crime location history

--- safespace_backend/backend.py
+++ safespace_backend/backend.py
@@ -3,15 +3,16 @@ import json
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("safespace_backend\certificate.json")
+cred = credentials.Certificate("safespace_backend/certificate.json")
 firebase_admin = firebase_admin.initialize_app(cred)
 
 
 app = Flask(__name__)
 currentTokens = []
+crime_location_history = []
 @app.route("/")
 def default():
-  return " "
+  return "App is Up"
 
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
@@ -22,6 +23,13 @@ def subscribe():
     print(currentTokens)
     return " "
 
+@app.route('/send_crime_location_history', methods=['GET'])
+def send_crime_location_history():
+    print(crime_location_history)
+    crime_location_dict = {"crime location history": crime_location_history}
+    
+    return crime_location_dict
+
 @app.route('/receive_signal', methods=['POST'])
 def receive_signal(): #comes in as json
     received_json = json.loads(request.data.decode('utf-8'))
@@ -29,6 +37,10 @@ def receive_signal(): #comes in as json
     
     current_tokens_copy = currentTokens[:]
     del current_tokens_copy[currentTokens.index(received_token)]
+
+    crime_location = [float(received_json["data"]["lat"]), float((received_json["data"]["long"]))]
+    if crime_location not in crime_location_history:
+        crime_location_history.append(crime_location)
 
     message = messaging.MulticastMessage(
         notification=messaging.Notification(
